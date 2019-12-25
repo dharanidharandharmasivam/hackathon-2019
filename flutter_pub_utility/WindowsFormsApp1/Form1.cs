69f368c3b6f2d8232f6587c263791a74c0ea1492
AzureAD\DharanidharanDharmas <dharanidharan.dharmasivam@syncfusion.com>
2019-12-25 23:47:14 +0530

    Committed project

---
+++ flutter_pub_utility/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string directory = @"D:\Hackathon-2019\sample_project\sample_proj";
+            string command = "flutter --version";
+            executePowershell(directory, command);
+        }
+
+        private void executePowershell(string location, string command) {
+            var Restoreprocess = new System.Diagnostics.Process();
+            Restoreprocess.StartInfo.FileName = "cmd.exe";
+            Restoreprocess.StartInfo.WorkingDirectory = location;
+            Restoreprocess.StartInfo.Arguments = "/c "+ command;
+            Restoreprocess.StartInfo.UseShellExecute = false;
+            Restoreprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Restoreprocess.StartInfo.Verb = "runas";
+            Restoreprocess.Start();
+            Restoreprocess.WaitForExit();
+        }
+    }
+}
