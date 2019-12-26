407465ee858b886efd014ab6ea91ea5b6a4fbc5d
sumathijayaraj <sumathijayaraj@syncfusion.com>
2019-12-26 11:19:36 +0530

    WPF project included

---
+++ flutter_pub_utility_wpf/Flutter_Publish_Utility/Flutter_Publish_Utility/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flutter_Publish_Utility
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PowerShellButton_Click(object sender, RoutedEventArgs e)
+        {
+            string directory = @"D:\src\flutterSB_MR\flutter_examples\flutter_examples";
+            string command = "flutter --version";
+            RunPowerShell(directory, command);
+        }
+
+        private void RunPowerShell(string location, string command)
+        {
+            var Restoreprocess = new System.Diagnostics.Process();
+            Restoreprocess.StartInfo.FileName = "cmd.exe";
+            Restoreprocess.StartInfo.WorkingDirectory = location;
+            Restoreprocess.StartInfo.Arguments = "/c " + command;
+            Restoreprocess.StartInfo.UseShellExecute = false;
+            Restoreprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Restoreprocess.StartInfo.Verb = "runas";
+            Restoreprocess.Start();
+            Restoreprocess.WaitForExit();
+        }
+    }
+}
