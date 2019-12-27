8d3cae35cfb3f6899e0666376e8bdb8868cff38b
rokesh karthikeyan <rokesh.karthikeyan@syncfusion.com>
2019-12-27 17:11:28 +0530

    MailBox added

---
+++ flutter_pub_utility_wpf/Flutter_Publish_Utility/Flutter_Publish_Utility/MailWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flutter_Publish_Utility
+{
+    /// <summary>
+    /// Interaction logic for MailWindow.xaml
+    /// </summary>
+    public partial class MailWindow : Window
+    {
+        public String Email = "";
+        public MailWindow()
+        {
+            InitializeComponent();
+            Email = "";
+        }
+
+
+
+        private void Answer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Email = (sender as TextBox).Text;
+        }
+
+
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
